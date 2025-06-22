bd1cb6d64bb94ecd5178b57298f6ab4213c6cd1b
Andrew Boylan <andreweboylan@gmail.com>
2025-06-22 07:57:02 +0000

    Updated audio directory to prod_2025_06_22 and sentiment classification

--- config/constants.py
+++ config/constants.py
@@ -1,6 +1,6 @@
 ######## Directories #########
 ROOT_DIR = "/home/denial/denial_payphone/payphone"
-AUDIO_DIR = "/home/denial/denial_payphone/payphone/audio_files/prod_2025_06_13"
+AUDIO_DIR = "/home/denial/denial_payphone/payphone/audio_files/prod_2025_06_22"
 SESSIONS_ROOT = "/home/denial/denial_payphone/payphone/sessions"
 DATABASE_DIR = "/home/denial/denial_payphone/payphone/database"
 
@@ -28,7 +28,7 @@ MAX_INITIAL_SILENCE_SEC = 13.0  # Max silence before prompting again or disconne
 AUDIO_SILENCE_THRESHOLD = 0.02  # RMS amplitude below this is considered silence (0.0-1.0)
 
 ######## Proximity sensor constants #########
-PROXIMITY_THRESHOLD = 30       # >= this is ON HOOK; < this is OFF HOOK
+PROXIMITY_THRESHOLD = 25       # >= this is ON HOOK; < this is OFF HOOK
 OFF_HOOK_REQUIRED_DURATION = 3.0  # seconds proximity must stay low to count as off-hook
 ON_HOOK_REQUIRED_DURATION = 3.0   # seconds proximity must stay high to count as on-hook
 POLL_INTERVAL = 0.1               # seconds between sensor polls
